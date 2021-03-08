7fe711a34573662c3253584902c21326f2c85b6f
PuBHARGAVI <saibhargavi.puvvada@gmail.com>
2021-03-08 18:56:29 +0530

    Computed and Tested Area Functionality For Rectangle Class

--- src/main/java/Rectangle/Rectangle.java
+++ src/main/java/Rectangle/Rectangle.java
@@ -7,10 +7,4 @@ public int computeArea(int length,int breadth) {
 	}
 	return breadth*length;
 	}
-public int computePerimeter(int length,int breadth) {
-	if(length<0 || breadth<0) {
-		return -1;
-	}
-	return 2*(breadth+length);
-	}
 }
